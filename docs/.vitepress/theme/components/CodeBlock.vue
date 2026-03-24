2526ffd83b73f89265663f51373bd3bd73f02cf6
AchintyaAkula <154371377+AchintyaAkula@users.noreply.github.com>
2026-03-24 15:18:37 -0700

    Update CodeBlock.vue

--- docs/.vitepress/theme/components/CodeBlock.vue
+++ docs/.vitepress/theme/components/CodeBlock.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="custom-code-group">
+  <div class="custom-code-block-wrapper">
+    <div v-if="description" class="insertion-point">
+      <div class="code-description" v-html="description"></div>
+    </div>
     <slot></slot>
-    <div v-if="description" class="code-description" v-html="description"></div>
   </div>
 </template>
 
@@ -10,9 +12,7 @@ defineProps(['description'])
 </script>
 
 <style scoped>
-.custom-code-group {
-  display: flex;
-  flex-direction: column;
+.custom-code-block-wrapper {
   margin: 16px 0;
   border: 1px solid var(--vp-c-divider);
   border-radius: 8px;
@@ -20,29 +20,38 @@ defineProps(['description'])
   background-color: var(--vp-code-block-bg);
 }
 
-/* Reordering logic to put description under tabs */
+/* 1. Force the internal VitePress group to be a flex container */
 :deep(.vp-code-group) {
   display: flex !important;
   flex-direction: column !important;
-  margin: 0 !important;
 }
 
-:deep(.tabs) { 
-  order: 1 !important; 
+/* 2. TAB BAR: Set to first position */
+:deep(.vp-code-group .tabs) {
+  order: 1 !important;
+  margin-bottom: 0 !important;
   border-bottom: 1px solid var(--vp-c-divider) !important;
 }
 
-.code-description { 
-  order: 2 !important; 
+/* 3. DESCRIPTION: We "teleport" it visually using order 2 */
+/* We move the whole insertion-point into the flex flow of the group */
+.insertion-point {
+  display: contents; /* Makes this div "invisible" so child can use parent flex */
+}
+
+.code-description {
+  order: 2 !important;
   padding: 12px 16px;
   font-size: 0.9em;
   color: var(--vp-c-text-1);
   background-color: var(--vp-c-bg-soft);
   border-bottom: 1px solid var(--vp-c-divider);
+  line-height: 1.5;
 }
 
-:deep(div[class*='language-']) { 
-  order: 3 !important; 
+/* 4. CODE CONTENT: Set to third position */
+:deep(.vp-code-group div[class*='language-']) {
+  order: 3 !important;
   border-radius: 0 !important;
   margin: 0 !important;
 }
